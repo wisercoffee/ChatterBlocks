82a9a8348d1d451e036ed7fd37d6bf78334e62eb
Hao Su <me@haosu.org>
2011-04-06 14:35:44 -0700

    another commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatterBlocks::Application.config.secret_token = '4ad913ce74f954b4d018188382c50ed52cb1cd2d7975b00869798919d1764c5fc210ece6ff621c473749224b262b69fa6e7fb388a3d3566add65ec7909d33f39'
